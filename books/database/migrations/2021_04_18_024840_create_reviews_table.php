c30ec3a290dae4c9f14b3c72800347a38476d64f
tame <78590091+1129-tame@users.noreply.github.com>
2021-04-18 19:16:54 +0900

    first_commit

---
+++ books/database/migrations/2021_04_18_024840_create_reviews_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            // レビューのテーブル設計
+            $table->bigIncrements('id');
+            // $table->bigInteger('user_id')->unsigned(); これが入ると重複するためエラーになる
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('title');
+            $table->longText('body');
+            $table->string('image')->nullable(); //画像投稿しない場合に備えてNULL許可しておく
+            $table->tinyInteger('status')->default(1)->comment('0=下書き, 1=アクティブ, 2=削除済み');
+            $table->timestamp('update_at')->useCurrentOnUpdate();
+            $table->timestamp('created_at')->useCurrent(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
