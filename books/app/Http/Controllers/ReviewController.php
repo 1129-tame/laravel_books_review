30e6df341b0b78f7cda6e504cef42259b985c360
tame <78590091+1129-tame@users.noreply.github.com>
2021-04-23 09:51:33 +0900

    5-5_commmit_validation

--- books/app/Http/Controllers/ReviewController.php
+++ books/app/Http/Controllers/ReviewController.php
@@ -20,7 +20,21 @@ class ReviewController extends Controller
 
     public function store(Request $request) {
         $post = $request->all();
-        $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body']];
+
+        //バリデーション
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+            'image' => 'mimes:jpeg, png, jpg, gif, svg|max:2048',
+        ]);
+
+        if ($request->hasFile('image')) {
+            $request->file('image')->store('/public/images');
+            $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body'], 'image' => $request->file('image')->hashName()];
+        } else {
+            $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body']];
+        }
+        
         // dd($post);
         // dd($data);
         Review::insert($data);
